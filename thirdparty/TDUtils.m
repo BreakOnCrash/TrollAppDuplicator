71f19f44b45c29925f69bb9cff48be51f12b5d95
ac0d3r <admin@imipy.com>
2024-08-23 19:39:58 +0800

    Fix read mach header 32&64

--- thirdparty/TDUtils.m
+++ thirdparty/TDUtils.m
@@ -114,25 +114,37 @@ NSArray *duplicatedFileList(void) {
 BOOL isArchitectureEncrypted(FILE *binary, uint32_t offset) {
     fseek(binary, offset, SEEK_SET);
 
-    struct mach_header_64 header;
-    fread(&header, sizeof(struct mach_header_64), 1, binary);
+    // 读取 mach_header 的前四个字节以确定是 32 位还是 64 位
+    uint32_t magic;
+    fread(&magic, sizeof(uint32_t), 1, binary);
 
-    if (header.magic != MH_MAGIC_64 && header.magic != MH_MAGIC) {
-        return NO;
+    if (magic != MH_MAGIC && magic != MH_CIGAM && magic != MH_MAGIC_64 && magic != MH_CIGAM_64) {
+        return NO;  // 非有效的 Mach-O 文件
+    }
+    BOOL is64Bit = (magic == MH_MAGIC_64 || magic == MH_CIGAM_64);
+
+    if (is64Bit) {
+        struct mach_header_64 header;
+        fseek(binary, offset, SEEK_SET);
+        fread(&header, sizeof(struct mach_header_64), 1, binary);
+        fseek(binary, offset + sizeof(struct mach_header_64), SEEK_SET);
+    } else {
+        struct mach_header header;
+        fseek(binary, offset, SEEK_SET);
+        fread(&header, sizeof(struct mach_header), 1, binary);
+        fseek(binary, offset + sizeof(struct mach_header), SEEK_SET);
     }
 
-    fseek(binary, offset + sizeof(struct mach_header_64), SEEK_SET);
-
     struct load_command lc;
-    for (uint32_t i = 0; i < header.ncmds; i++) {
+    for (uint32_t i = 0; i < (is64Bit ? ((struct mach_header_64 *)(&magic))->ncmds:((struct mach_header *)(&magic))->ncmds);i++) {
         fread(&lc, sizeof(struct load_command), 1, binary);
 
-        if (lc.cmd == LC_ENCRYPTION_INFO_64) {
+        if (lc.cmd == LC_ENCRYPTION_INFO_64 && is64Bit) {
             struct encryption_info_command_64 encryptCmd;
             fseek(binary, -sizeof(struct load_command), SEEK_CUR);
             fread(&encryptCmd, sizeof(struct encryption_info_command_64), 1, binary);
             return encryptCmd.cryptid != 0;
-        } else if (lc.cmd == LC_ENCRYPTION_INFO) {
+        } else if (lc.cmd == LC_ENCRYPTION_INFO && !is64Bit) {
             struct encryption_info_command encryptCmd;
             fseek(binary, -sizeof(struct load_command), SEEK_CUR);
             fread(&encryptCmd, sizeof(struct encryption_info_command), 1, binary);
